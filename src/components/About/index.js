64d35bd7b352e9b7d0d20376de7405df93ed79cf
serge2233 <serge5555@hotmail.com>
2022-06-25 20:43:03 -0700

    bio update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
         <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
         <p>
-        Hello, my name is Serge, I'm 23 years old and I speak two languages: Russian and English. I always had an eagerness to learn new languages, and I once came up to computer languages, which took me up to learn something completely new and change my career now! I took a course at UC Davis Coding Bootcamp, and now I am a Full Stack Web Developer!
+        Hello, my name is Serge; 23 years old, and I speak fluently Russian and English. Recently I had a desire to learn new languages, and once I examined computer languages, I felt that I found what i was searching for. In order to be fluent in web development, I took a course at UC Davis bootcamp, and after completing this course I received assurance in making this new step in my career.
         </p>
       </div>
     </section>
